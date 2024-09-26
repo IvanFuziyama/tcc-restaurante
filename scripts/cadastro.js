c56203d8bc7852be45d520aef46845e17fe820ee
Luiz Silva. <160427605+luizOASSilva@users.noreply.github.com>
2024-09-26 16:50:19 -0300

    estruturadando cadastro

---
+++ scripts/cadastro.js
@@ -0,0 +1,5 @@
+function Cadastrar(){
+    let email = document.querySelector('input#email')
+    let senha = document.querySelector('input#senha')
+    let sconfirma = document.querySelector('input#confirmar-senha')
+}
--- scripts/login.js
+++ scripts/login.js
@@ -1,7 +1,8 @@
 function Entrar(){
-    let nome = document.getElementById('email').value
-    let senha = document.getElementById('senha').value
-    if(nome == "" || senha == ""){
+    let email = document.querySelector('input#email').value
+    let senha = document.querySelector('input#senha').value
+
+    if(email == "" || senha == ""){
         alert("Coloque as informações")
     }else{
         window.location.href="../paginas-adm/adm.html"
