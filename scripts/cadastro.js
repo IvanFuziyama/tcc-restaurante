53645fbf1ad48ebd1e8331797b563ad15ac18444
IvanFuziyama <ivanseiji21@gmail.com>
2024-10-11 14:07:26 -0300

    cadastro firebase

--- scripts/cadastro.js
+++ scripts/cadastro.js
@@ -1,5 +1,38 @@
-function Cadastrar(){
-    let email = document.querySelector('input#email')
-    let senha = document.querySelector('input#senha')
-    let sconfirma = document.querySelector('input#confirmar-senha')
+    
+const firebaseConfig = {
+    apiKey: "AIzaSyCbgUdVBog-R0DmIZi0mG51_uuhDfnWj4c",
+    authDomain: "yaki-bb90f.firebaseapp.com",
+    projectId: "yaki-bb90f",
+    storageBucket: "yaki-bb90f.appspot.com",
+    messagingSenderId: "1025023938370",
+    appId: "1:1025023938370:web:de4f3190bd0d76d36102db",
+    measurementId: "G-Y3NNNQ1004"
+  };
+  
+  // Initialize Firebase
+  const app = firebase.initializeApp(firebaseConfig);
+  const auth = firebase.auth();
+
+  function cadastrar(){
+    const email = document.querySelector('input#email').value;
+    const senha = document.querySelector('input#senha').value;
+
+    const confirmarSenha = document.querySelector('input#confirmar-senha').value;
+    if (senha !== confirmarSenha) {
+        alert("As senhas não coincidem.");
+        return;
+    }
+
+    auth.createUserWithEmailAndPassword(email, senha)
+    .then((userCredential) => {
+        // Sucesso ao criar usuário
+        alert("Seus dados foram cadastrados com sucesso!");
+        document.querySelector('input#email').value = '';
+        document.querySelector('input#senha').value = '';
+        document.querySelector('input#confirmar-senha').value = '';
+    })
+    .catch((error) => {
+        // Falha no cadastro
+        alert("Falha ao cadastrar: " + error.message);
+    });
 }
--- scripts/login.js
+++ scripts/login.js
@@ -1,5 +1,18 @@
 //fazer com firebase
-
+const firebaseConfig = {
+    apiKey: "AIzaSyCbgUdVBog-R0DmIZi0mG51_uuhDfnWj4c",
+    authDomain: "yaki-bb90f.firebaseapp.com",
+    projectId: "yaki-bb90f",
+    storageBucket: "yaki-bb90f.appspot.com",
+    messagingSenderId: "1025023938370",
+    appId: "1:1025023938370:web:de4f3190bd0d76d36102db",
+    measurementId: "G-Y3NNNQ1004"
+  };
+  
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+  const analytics = getAnalytics(app);
+  
 function verSenha(){
     const senhaTxt = document.querySelector('input#senha')
     const senhaVer = document.querySelector('span#ver-senha')
