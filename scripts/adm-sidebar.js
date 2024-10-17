241ec3b6fe3dc294754826dbc562747b36688583
Ivan Fuziyama <IvanSeiji21@gmail.com>
2024-10-17 11:01:18 -0300

    sair

--- scripts/adm-sidebar.js
+++ scripts/adm-sidebar.js
@@ -1,3 +1,9 @@
 document.querySelector('#side_botao').addEventListener('click', function(){
     document.querySelector('#sidebar').classList.toggle('open_sidebar')
 })
+
+function sair() {
+    if (confirm('Você realmente deseja sair?')) {
+        window.location.href = 'login-adm.html';
+    }
+}
